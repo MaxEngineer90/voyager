c5f14c776e347583348cabe8e381421980acba98
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-02-09 22:35:44 +0100

    rename action-menu folder

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -4,20 +4,21 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTab, MatTabContent, MatTabGroup, MatTabLink, MatTabNav, MatTabNavPanel} from "@angular/material/tabs";
 import {ThemePalette} from "@angular/material/core";
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {NgForOf} from "@angular/common";
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput, MatInputModule} from "@angular/material/input";
 import {FormControl} from "@angular/forms";
 import {PathComponent} from "./path/path.component";
-import {ActionMenuBarComponent} from "./action-menu/action-menu-bar.component";
-import {MatSidenav} from "@angular/material/sidenav";
+import {ActionMenuBarComponent} from "./action-menu-bar/action-menu-bar.component";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
 
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTabGroup, MatTab, MatTabNav, MatFormFieldModule, MatInputModule, MatTabLink, PathComponent, ActionMenuBarComponent, MatSidenav],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTabGroup, MatTab, MatTabNav, MatFormFieldModule, MatInputModule, MatTabLink, PathComponent, ActionMenuBarComponent, MatSidenav, MatSidenavContent, RouterOutlet, MatNavList, MatListItem, MatSidenavContainer],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
